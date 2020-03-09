ab62b356dae83caac493cbf6ea3ac86b7add7bdd
bikiniweekend <bikiniweekend@hotmail.com>
2020-03-09 21:52:01 +0100

    new update

--- src/components/FallIntoYouButton.jsx
+++ src/components/FallIntoYouButton.jsx
@@ -3,17 +3,18 @@
  * Button dynamic based on 'Cosmic Gate & JES - Fall Into You (Lyric Video)'
  * https://www.youtube.com/watch?v=f1x16SkHizQ
  */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './FallIntoYouButton.css';
 
-export default class FiuButton extends Component {
+export default class FallIntoYouButton extends Component {
 	constructor(props) {
-
+		super(props);
 	}
 
 	render() {
 		return (
-			<div>
-				
+			<div className="fall-into-you-button">
+				Texto
 			</div>
 		)
 	}
