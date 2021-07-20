c8f8e85020a2bbf2f7891ddfdb964544f485f9dc
lorenzo.portillo.dev@gmail.com <lorenzo.portillo.dev@gmail.com>
2021-07-20 22:05:20 +0200

    KHW-20 Loading scene development

--- src/app/scene/scene-loading.ts
+++ src/app/scene/scene-loading.ts
@@ -1,10 +1,10 @@
 import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder';
+import { Mesh } from '@babylonjs/core/Meshes/mesh';
 import { Color3, Color4 } from '@babylonjs/core/Maths/math.color';
 import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
 import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera';
 import { Vector3 } from '@babylonjs/core/Maths/math.vector';
 import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
-import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader';
 import { Control } from '@babylonjs/gui/2D/controls/control';
 import { TextBlock } from '@babylonjs/gui/2D/controls/textBlock';
 
@@ -13,15 +13,23 @@ import { DimensionsWH } from '../../core/models/dimensions-wh';
 import { GUI } from '../../core';
 
 export class SceneIntro extends Scene {
+    private camera: UniversalCamera;
+    private light: HemisphericLight;
     private logo: Sprite;
+    private sun: Sprite;
+    private earth: Mesh;
 
+    // ******************
     // Debug 8a8f delete
+    gui: GUI;
     textCanvasSize: TextBlock;
+    // ******************
 
     onLoad(): void {
-        // if (this.isDevelopmentMode) {
-        const gui = new GUI(this);
-        this.textCanvasSize = gui.newTextBlock();
+        // ******************
+        // Debug
+        this.gui = new GUI(this);
+        this.textCanvasSize = this.gui.newTextBlock();
         this.textCanvasSize.left = 0;
         this.textCanvasSize.top = 300;
         this.textCanvasSize.width = '200px';
@@ -32,32 +40,46 @@ export class SceneIntro extends Scene {
         this.textCanvasSize.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
         this.textCanvasSize.text = 'TEXT BLOCK';
         this.textCanvasSize.color = '#fcf403';
-        // }
+        // ******************
 
-        SceneLoader.Append('./assets/mesh/', 'world3d.babylon', this.babylonjs, (scene) => {
-            // do something with the scene
-        });
+        // Fixed camera
+        this.camera = new UniversalCamera('camera', new Vector3(-3, 0, 0), this.babylonjs);
+        this.camera.target = new Vector3(1, 0, 0);
+        this.camera.inputs.clear();
+        this.camera.minZ = 0.01; // Let it go closer to the earth (reduce distance with near clipping plane)
+        this.camera.attachControl(this.canvas, true);
 
-        const camera: UniversalCamera = new UniversalCamera('Camera', new Vector3(-3, 0, 0), this.babylonjs);
-        camera.target = new Vector3(1, 0, 0);
-        camera.inputs.clear();
-        camera.attachControl(this.canvas, true);
-
-        const light1: HemisphericLight = new HemisphericLight('light1', new Vector3(1, 0, 0), this.babylonjs);
+        // Light in front of scene
+        this.light = new HemisphericLight('light', new Vector3(1, 0, 0), this.babylonjs);
 
         const flatMaterial = new StandardMaterial('', this.babylonjs);
         flatMaterial.disableLighting = true;
         flatMaterial.emissiveColor = new Color3(0.13, 0.13, 0.13);
 
-        const sphere = MeshBuilder.CreateSphere('sphere', {});
-        sphere.material = flatMaterial;
-        sphere.position.x = -1;
-        sphere.position.y = -1;
+        this.earth = MeshBuilder.CreateSphere('earth', {
+            segments: 128,
+        });
+        this.earth.material = flatMaterial;
+        // this.earth.position.x = -2.6996; // Start
+        // this.earth.position.y = -0.4112;
+        this.earth.position.x = -2.7286; // End
+        this.earth.position.y = -0.4792;
 
         // Logo
-        this.logo = this.addSprite('./assets/sprites/logo.png', { width: 453, height: 115, numFrames: 59 });
-        // logo.setFrame(50);
+        this.logo = this.addSprite('logo', './assets/scene-loading/sprites/logo.png', { width: 453, height: 115, numFrames: 59 });
         this.logo.play(50, false);
+        this.logo.babylonjs.position.y = 0.9;
+        this.logo.babylonjs.position.z = 1.9;
+
+        // Sun
+        this.sun = this.addSprite('sun', './assets/scene-loading/sprites/sun.png', { width: 270, height: 270, numFrames: 1 });
+        this.sun.setFrame(0);
+        // this.sun.babylonjs.position.y = 0.2699;
+        // this.sun.babylonjs.position.z = -3.4699; // Start
+        // this.sun.setScale(1.15);
+        this.sun.babylonjs.position.y = 0.8;
+        this.sun.babylonjs.position.z = -1.9; // End
+        this.sun.setScale(0.7);
 
         // const actionManager: ActionManager = new ActionManager(this.babylonjs)
         // actionManager.registerAction()
@@ -74,82 +96,53 @@ export class SceneIntro extends Scene {
     }
 
     onLoaded(canvasDimensions: DimensionsWH): void {
-        console.log('aki onLoaded, canvas dimensions:', canvasDimensions);
         this.textCanvasSize.text = `Canvas: ${canvasDimensions.width} x ${canvasDimensions.height}`;
 
         // Setup the scene
         this.babylonjs.clearColor = new Color4(0.19, 0.19, 0.19, 1.0);
 
-        // Get scene objects
-        const World3D = this.babylonjs.getMeshByID('Icosphere');
-        console.log('aki World3D:', World3D);
-        World3D.visibility = 0;
-
         // Attach imported camera to canvas inputs
-        // scene.activeCamera.attachControl(canvas);
+        this.babylonjs.activeCamera.attachControl(this.canvas);
 
         // Add subscriptions
         this.subscribeCanvasResize();
 
-        // Inputs
-        let lastPointerX: number;
-        let lastPointerY: number;
-        let pointerDiffX: number;
-        let pointerDiffY: number;
-        let click: number = 0;
-        let zoom: boolean;
-
+        // Input subscriptions
         this.canvas.addEventListener('keydown', (event) => {
-            console.log('aki keydown', event.key, event.code);
-            if (event.key === 'ArrowLeft') {
-                this.logo.babylonjs.position.z += 0.01;
-                console.log('aki position:', this.logo.babylonjs.position.x, this.logo.babylonjs.position.y, this.logo.babylonjs.position.z);
+            console.log('aki keydown', event.code, event.code);
+            const inc = event.altKey ? (event.ctrlKey ? 0.0001 : 0.001) : 0.01;
+            if (event.code === 'Numpad4') {
+                this.sun.babylonjs.position.z += inc;
+                console.log('aki position:', this.sun.babylonjs.position.x, this.sun.babylonjs.position.y, this.sun.babylonjs.position.z);
             }
-            if (event.key === 'ArrowRight') {
-                this.logo.babylonjs.position.z -= 0.01;
-                console.log('aki position:', this.logo.babylonjs.position.x, this.logo.babylonjs.position.y, this.logo.babylonjs.position.z);
+            if (event.code === 'Numpad6') {
+                this.sun.babylonjs.position.z -= inc;
+                console.log('aki position:', this.sun.babylonjs.position.x, this.sun.babylonjs.position.y, this.sun.babylonjs.position.z);
             }
-            if (event.key === 'ArrowUp') {
-                this.logo.babylonjs.position.y += 0.01;
-                console.log('aki position:', this.logo.babylonjs.position.x, this.logo.babylonjs.position.y, this.logo.babylonjs.position.z);
+            if (event.code === 'Numpad8') {
+                this.sun.babylonjs.position.y += inc;
+                console.log('aki position:', this.sun.babylonjs.position.x, this.sun.babylonjs.position.y, this.sun.babylonjs.position.z);
             }
-            if (event.key === 'ArrowDown') {
-                this.logo.babylonjs.position.y -= 0.01;
-                console.log('aki position:', this.logo.babylonjs.position.x, this.logo.babylonjs.position.y, this.logo.babylonjs.position.z);
+            if (event.code === 'Numpad2') {
+                this.sun.babylonjs.position.y -= inc;
+                console.log('aki position:', this.sun.babylonjs.position.x, this.sun.babylonjs.position.y, this.sun.babylonjs.position.z);
             }
+            if (event.code === 'Numpad9') {
+                // this.sun.babylonjs.position.x += inc;
+                this.sun.setScale(this.sun.getScale() - inc);
+                console.log('aki position:', this.sun.babylonjs.position.x, this.sun.babylonjs.position.y, this.sun.babylonjs.position.z);
+            }
+            if (event.code === 'Numpad3') {
+                // this.sun.babylonjs.position.x -= inc;
+                this.sun.setScale(this.sun.getScale() + inc);
+                console.log('aki position:', this.sun.babylonjs.position.x, this.sun.babylonjs.position.y, this.sun.babylonjs.position.z);
+            }
+            console.log(this.sun.getScale());
         });
 
-        this.canvas.addEventListener('pointermove', () => {
-            // console.log(Vector3.Zero(), 'aki MOUSE MOVE', this.babylonjs.pointerX, this.babylonjs.pointerY, click);
-
-            if (click === 1) {
-                if (zoom) {
-                    zoom = false;
-                } else {
-                    pointerDiffX = lastPointerX - this.babylonjs.pointerX;
-                    pointerDiffY = lastPointerY - this.babylonjs.pointerY;
-
-                    World3D.rotateAround(Vector3.Zero(), new Vector3(0, 1, 0), pointerDiffX * 0.005);
-                    World3D.rotateAround(Vector3.Zero(), new Vector3(0, 0, -1), pointerDiffY * 0.005);
-                }
-            }
-
-            lastPointerX = this.babylonjs.pointerX;
-            lastPointerY = this.babylonjs.pointerY;
-        });
-        this.canvas.addEventListener('pointerdown', () => {
-            click++;
-            if (click > 1) {
-                zoom = true;
-            }
-            lastPointerX = this.babylonjs.pointerX;
-            lastPointerY = this.babylonjs.pointerY;
-            // console.log('Mouse DOWN!');
-        });
-        this.canvas.addEventListener('pointerup', () => {
-            click--;
-            // console.log('Mouse UP!');
-        });
+        this.canvas.addEventListener('pointermove', () => {});
+        this.canvas.addEventListener('pointerdown', () => {});
+        this.canvas.addEventListener('pointerup', () => {});
     }
 
     subscribeCanvasResize(): void {
---
+++ src/app/scene/scene-world3d.ts
@@ -0,0 +1,166 @@
+import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder';
+import { Color3, Color4 } from '@babylonjs/core/Maths/math.color';
+import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
+import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera';
+import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
+import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader';
+import { Control } from '@babylonjs/gui/2D/controls/control';
+import { TextBlock } from '@babylonjs/gui/2D/controls/textBlock';
+
+import { Scene, Sprite } from '../../core';
+import { DimensionsWH } from '../../core/models/dimensions-wh';
+import { GUI } from '../../core';
+
+export class SceneIntro extends Scene {
+    private logo: Sprite;
+
+    // ******************
+    // Debug 8a8f delete
+    gui: GUI;
+    textCanvasSize: TextBlock;
+    // ******************
+
+    onLoad(): void {
+        // ******************
+        // Debug
+        this.gui = new GUI(this);
+        this.textCanvasSize = this.gui.newTextBlock();
+        this.textCanvasSize.left = 0;
+        this.textCanvasSize.top = 300;
+        this.textCanvasSize.width = '200px';
+        this.textCanvasSize.height = '40px';
+        this.textCanvasSize.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+        this.textCanvasSize.textHorizontalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+        this.textCanvasSize.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+        this.textCanvasSize.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+        this.textCanvasSize.text = 'TEXT BLOCK';
+        this.textCanvasSize.color = '#fcf403';
+        // ******************
+
+        SceneLoader.Append('./assets/mesh/', 'world3d.babylon', this.babylonjs, (scene) => {
+            // do something with the scene
+        });
+
+        const camera: UniversalCamera = new UniversalCamera('Camera', new Vector3(-3, 0, 0), this.babylonjs);
+        camera.target = new Vector3(1, 0, 0);
+        camera.inputs.clear();
+        camera.attachControl(this.canvas, true);
+
+        const light1: HemisphericLight = new HemisphericLight('light1', new Vector3(1, 0, 0), this.babylonjs);
+
+        const flatMaterial = new StandardMaterial('', this.babylonjs);
+        flatMaterial.disableLighting = true;
+        flatMaterial.emissiveColor = new Color3(0.13, 0.13, 0.13);
+
+        const sphere = MeshBuilder.CreateSphere('sphere', {});
+        sphere.material = flatMaterial;
+        sphere.position.x = -1;
+        sphere.position.y = -1;
+
+        // Logo
+        this.logo = this.addSprite('', './assets/sprites/logo.png', { width: 453, height: 115, numFrames: 59 });
+        // logo.setFrame(50);
+        this.logo.play(50, false);
+
+        // const actionManager: ActionManager = new ActionManager(this.babylonjs)
+        // actionManager.registerAction()
+
+        // const input: InputManager = new InputManager(this.babylonjs)
+        // input.attachControl()
+
+        /*
+        const sphere: Mesh = MeshBuilder.CreateSphere(
+            'sphere',
+            { diameter: 1 },
+            this.babylonjs
+        );*/
+    }
+
+    onLoaded(canvasDimensions: DimensionsWH): void {
+        console.log('aki onLoaded, canvas dimensions:', canvasDimensions);
+        this.textCanvasSize.text = `Canvas: ${canvasDimensions.width} x ${canvasDimensions.height}`;
+
+        // Setup the scene
+        this.babylonjs.clearColor = new Color4(0.19, 0.19, 0.19, 1.0);
+
+        // Get scene objects
+        const World3D = this.babylonjs.getMeshByID('Icosphere');
+        console.log('aki World3D:', World3D);
+        World3D.visibility = 0;
+
+        // Attach imported camera to canvas inputs
+        // scene.activeCamera.attachControl(canvas);
+
+        // Add subscriptions
+        this.subscribeCanvasResize();
+
+        // Inputs
+        let lastPointerX: number;
+        let lastPointerY: number;
+        let pointerDiffX: number;
+        let pointerDiffY: number;
+        let click: number = 0;
+        let zoom: boolean;
+
+        this.canvas.addEventListener('keydown', (event) => {
+            console.log('aki keydown', event.key, event.code);
+            if (event.key === 'ArrowLeft') {
+                this.logo.babylonjs.position.z += 0.01;
+                console.log('aki position:', this.logo.babylonjs.position.x, this.logo.babylonjs.position.y, this.logo.babylonjs.position.z);
+            }
+            if (event.key === 'ArrowRight') {
+                this.logo.babylonjs.position.z -= 0.01;
+                console.log('aki position:', this.logo.babylonjs.position.x, this.logo.babylonjs.position.y, this.logo.babylonjs.position.z);
+            }
+            if (event.key === 'ArrowUp') {
+                this.logo.babylonjs.position.y += 0.01;
+                console.log('aki position:', this.logo.babylonjs.position.x, this.logo.babylonjs.position.y, this.logo.babylonjs.position.z);
+            }
+            if (event.key === 'ArrowDown') {
+                this.logo.babylonjs.position.y -= 0.01;
+                console.log('aki position:', this.logo.babylonjs.position.x, this.logo.babylonjs.position.y, this.logo.babylonjs.position.z);
+            }
+        });
+
+        this.canvas.addEventListener('pointermove', () => {
+            // console.log(Vector3.Zero(), 'aki MOUSE MOVE', this.babylonjs.pointerX, this.babylonjs.pointerY, click);
+
+            if (click === 1) {
+                if (zoom) {
+                    zoom = false;
+                } else {
+                    pointerDiffX = lastPointerX - this.babylonjs.pointerX;
+                    pointerDiffY = lastPointerY - this.babylonjs.pointerY;
+
+                    World3D.rotateAround(Vector3.Zero(), new Vector3(0, 1, 0), pointerDiffX * 0.005);
+                    World3D.rotateAround(Vector3.Zero(), new Vector3(0, 0, -1), pointerDiffY * 0.005);
+                }
+            }
+
+            lastPointerX = this.babylonjs.pointerX;
+            lastPointerY = this.babylonjs.pointerY;
+        });
+        this.canvas.addEventListener('pointerdown', () => {
+            click++;
+            if (click > 1) {
+                zoom = true;
+            }
+            lastPointerX = this.babylonjs.pointerX;
+            lastPointerY = this.babylonjs.pointerY;
+            // console.log('Mouse DOWN!');
+        });
+        this.canvas.addEventListener('pointerup', () => {
+            click--;
+            // console.log('Mouse UP!');
+        });
+    }
+
+    subscribeCanvasResize(): void {
+        this.addSubscription(
+            this.coreSubscriptions.canvasResize.subscribe((dimensions) => {
+                this.textCanvasSize.text = `Canvas: ${dimensions.width} x ${dimensions.height}`;
+            })
+        );
+    }
+}
