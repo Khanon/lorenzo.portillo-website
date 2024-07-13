0d339b40c1966237b8a1806b3eedbc4e0b5d2bbf
development@khanon3d.com <development@khanon3d.com>
2024-07-13 18:48:12 +0200

    .

--- src/scenes/scene-intro/actors/robocillo/robocillo-actor.ts
+++ src/scenes/scene-intro/actors/robocillo/robocillo-actor.ts
@@ -1,17 +1,48 @@
 import {
   Actor,
   ActorInterface,
+  ActorSprite,
   Logger,
+  SpriteConstructor,
   SpriteInterface
 } from '@khanonjs/engine'
 
-import { RobocilloSprite } from './robocillo-sprite'
+import { RobocilloAnimationIds } from './robocillo-animation-ids'
+import { RobocilloKeyFrames } from './robocillo-keyframes'
 
-@Actor({
-  sprites: [RobocilloSprite]
-})
+@Actor()
 export class RobocilloActor extends ActorInterface<SpriteInterface> {
-  onLoaded(): void {
-    // Logger.trace('aki ActorRobocillo onLoaded')
+  @ActorSprite({
+    url: './assets/scene-intro/sprites/robocillo.png',
+    animations: [
+      { id: RobocilloAnimationIds.STOP_SIDE, delay: 75, frameStart: 0, frameEnd: 0, loop: false },
+      { id: RobocilloAnimationIds.PAPER_TAKE, delay: 75, frameStart: 8, frameEnd: 15, loop: false },
+      { id: RobocilloAnimationIds.PAPER_CHECK, delay: 75, frameStart: 16, frameEnd: 21, loop: false },
+      { id: RobocilloAnimationIds.PAPER_THROW, delay: 75, frameStart: 24, frameEnd: 27, loop: false },
+      { id: RobocilloAnimationIds.SIDE_TO_FRONT, delay: 75, frameStart: 32, frameEnd: 34, loop: false },
+      { id: RobocilloAnimationIds.FRONT_TO_SIDE, delay: 75, frameStart: 40, frameEnd: 42, loop: false },
+      { id: RobocilloAnimationIds.STOP_FRONT, delay: 75, frameStart: 48, frameEnd: 48, loop: false },
+      {
+        id: RobocilloAnimationIds.WALK,
+        delay: 75,
+        frameStart: 56,
+        frameEnd: 63,
+        loop: true,
+        keyFrames: [
+          {
+            id: RobocilloKeyFrames.FLOOR_CONTACT,
+            frames: [56, 60]
+          }
+        ]
+      },
+      { id: RobocilloAnimationIds.MOVE_HANDS, delay: 75, frameStart: 64, frameEnd: 66, loop: true },
+      { id: RobocilloAnimationIds.RAISE_HANDS, delay: 75, frameStart: 72, frameEnd: 74, loop: true },
+      { id: RobocilloAnimationIds.JUMP_FRONT, delay: 75, frameStart: 80, frameEnd: 85, loop: false }
+    ]
+  }) roboti: SpriteConstructor
+
+  onSpawn(): void {
+    // Logger.trace('aki ActorRobocillo onSpawn')
+    this.setBody(this.roboti)
   }
 }
