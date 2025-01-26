69eb2ea3376273cf86075bfcc824cdf90e1376ea
development@khanon3d.com <development@khanon3d.com>
2025-01-26 18:00:22 +0100

    .

--- src/scenes/scene-world/scene-world-state.ts
+++ src/scenes/scene-world/scene-world-state.ts
@@ -38,31 +38,46 @@ export class SceneWorldState extends SceneStateInterface {
   }) monster: MeshConstructor
 
   @Mesh({
-    url: '/assets/scene-world/meshes/character/character-test-monster.glb',
+    url: '/assets/scene-world/meshes/character/character-test-monster-opt.glb',
     animations: [
       { id: 'OldMan', loop: true },
-      { id: 'Walking', loop: true }
+      {
+        id: 'Walking',
+        loop: true,
+        keyFrames: [
+          { id: 'middle', frames: [32] },
+          { id: 'end', frames: [63] }
+        ]
+      }
     ],
     cloneByInstances: true
   }) monster1: MeshConstructor
 
   onStart() {
     this.switchCamera(SceneWorldCamera, {})
-    const peasant = this.scene.spawn.mesh(this.peasant, 2, (mesh, index) => {
+    /* const peasant = this.scene.spawn.mesh(this.peasant, 2, (mesh, index) => {
       mesh.position.z -= (index + 1) * 0.3
       mesh.playAnimation(index === 0 ? 'Taunt' : 'Dying')
     })
     const monster = this.scene.spawn.mesh(this.monster, 2, (mesh, index) => {
       mesh.position.z += (index + 1) * 0.3
       mesh.playAnimation(index === 0 ? 'OldMan' : 'Walking')
-    })
+    }) */
 
     const monster1 = this.scene.spawn.mesh(this.monster1, 2, (mesh, index) => {
-      mesh.position.z += (index + 4) * 0.3
-      mesh.playAnimation(index === 0 ? 'Walking' : 'OldMan', {
-        loop: false,
-        speedRatio: 0.1
+      // mesh.position.z += (index + 4) * 0.3
+      mesh.rotate(BABYLON.Axis.Y, index * -Math.PI, BABYLON.Space.WORLD)
+      mesh.playAnimation('Walking', {
+        // loop: true,
+        speedRatio: 1 - (0.8 * index)
       }, () => { Logger.trace('aki completed!') })
+      Logger.trace('aki keyframes', index)
+      mesh.subscribeToKeyframe('middle', () => {
+        Logger.trace('aki MIDDLE!', index)
+      })
+      mesh.subscribeToKeyframe('end', () => {
+        Logger.trace('aki END!', index)
+      })
     })
   }
 }
